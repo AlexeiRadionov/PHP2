90895cd1db3348427d7b4c8e668ec01aee172d04
AlexeyRadionov <radionov-aleksei@mail.ru>
2018-08-29 16:25:50 +0600

    создан класс наследник для вывода одного изображения

---
+++ Image.class.php
@@ -0,0 +1,41 @@
+<?php
+	class Image extends Gallery {
+		private $id;
+
+		public function getId() {
+			$this -> id;
+		}
+
+		public function setter($id) {
+			$this -> id = $id;
+		}
+
+		function __construct($id) {
+			$this -> setter($id);
+		}
+
+		public function getImagesContent($id){
+		    $sql = "SELECT `path_img`, `count_preview` FROM `images` WHERE id = " . $id;
+		    $images = $this -> getAssocResult($sql);
+
+			//В случае если изображения нет, вернем пустое значение
+		    $result = [];
+		    if(isset($images[0]))
+		        $result = $images[0];
+		    
+		    return $result;
+		}
+
+		public function countPreview($id) {
+		    $sql = "UPDATE `images` SET `count_preview` = `count_preview` + 1 WHERE id = " . $id;
+		    $this -> executeQuery($sql);
+		    
+		    $sql = "SELECT `count_preview` FROM `images` WHERE id = " . $id;
+		    $count = $this -> getAssocResult($sql);
+		    
+		    return $count[0]['count_preview']; 
+		}
+	}
+
+	$objImage = new Image($id);
+?>

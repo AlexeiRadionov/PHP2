3dd323332f17eba610e89138211f8d8887cf7d53
AlexeyRadionov <radionov-aleksei@mail.ru>
2018-09-02 02:00:31 +0600

    добавил функции для вывода страницы с каталогом через аякс

--- public/js/engine.js
+++ public/js/engine.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var countGoods = 5;
     $('.buy').on('click', function(){
         var id_good = $(this).attr("id");
         $.ajax({
@@ -38,6 +39,26 @@ $(document).ready(function(){
             dataType : "json"
         })
     });
+
+    $('.showGoods').on('click', function(){
+        countGoods += 5;
+        $.ajax({
+            url: "/catalog/addShowGoods/",
+            type: "POST",
+            data:{
+                countGoods: countGoods
+            },
+            dataType : "json",
+            success: function(answer){
+                if(answer.result == 1) {
+                    console.log(answer);
+                    addShowGoods(answer);
+                } else
+                    alert("Что-то пошло не так...");
+            },
+            error: function() {alert("Ошибка");}
+        })
+    });
 });
 
 function addProduct(data) { 
@@ -55,3 +76,17 @@ function removeProduct(data) {
     $('#sum').html('Общая сумма покупки: ' + data.sum);
 }
 
+function addShowGoods(data) {
+    var str = '';
+    delete data.result;
+    if (data.answer) {
+        delete data.answer;
+        $('.showGoods').attr('style', 'display:none');
+    }
+    
+    for(var good in data) {
+        //console.log(data[good]);
+        str += '<div data-id="'+data[good].id_image+'"><img src="../../img/small/'+data[good].path_img+'"><p>"'+data[good].description+'"</p><p>Цена: "'+data[good].price+'" рублей за 1шт.</p><form action="/image/" method="GET" target="_blank"><input type="hidden" name="id" value="'+data[good].id_image+'"><button>Подробнее</button></form><hr></div>';
+    }  
+    $('.goods').html(str);
+}

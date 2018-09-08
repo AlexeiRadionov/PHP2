06be3af8d74f2b83c4346a7704483024bfb76846
AlexeyRadionov <radionov-aleksei@mail.ru>
2018-09-08 14:24:44 +0600

    добавил комментарии

--- public/index.php
+++ public/index.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
-//подключаем все библиотеки
+//подключаем все классы
 include '../model/DataBase.class.php';
 include '../model/Gallery.class.php';
 function __autoload($className) {
 	include "../model/$className.class.php";
 }
+//подключаем контроллеры
 include '../controller/controllerMain.php';
 include '../controller/controllerAddFeedback.php';
 

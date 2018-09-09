894a2dc051d58544a2f44549284cf45d6db65771
AlexeyRadionov <radionov-aleksei@mail.ru>
2018-09-09 23:23:28 +0600

    обновил таблицы

--- data/gallery.sql
+++ data/gallery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Сен 09 2018 г., 14:41
+-- Время создания: Сен 09 2018 г., 20:14
 -- Версия сервера: 5.7.20
 -- Версия PHP: 5.6.32
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `basket` (
-  `id` int(11) NOT NULL,
+  `id_basket` int(11) NOT NULL,
   `id_session` text NOT NULL,
   `id_product` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
@@ -39,17 +39,8 @@ CREATE TABLE `basket` (
 -- Дамп данных таблицы `basket`
 --
 
-INSERT INTO `basket` (`id`, `id_session`, `id_product`, `quantity`) VALUES
-(1, '1jl8j3lr6roikqur3tc1mkdda4', 2, 1),
-(2, '1jl8j3lr6roikqur3tc1mkdda4', 13, 2),
-(3, '1jl8j3lr6roikqur3tc1mkdda4', 3, 1),
-(4, 'cr0g9m9q3km5olar727uf45im6', 12, 1),
-(6, 'cr0g9m9q3km5olar727uf45im6', 14, 1),
-(9, 'g78g44ur9j6trasur3g8oqhbd5', 2, 1),
-(11, 'tce66jjdu4jit06kudvlgp4703', 2, 1),
-(25, 'd7229flevpudcujujt06t34ss2', 2, 1),
-(33, '10dfq0pq2cpk0ht3ec0v5pnai3', 5, 2),
-(34, '10dfq0pq2cpk0ht3ec0v5pnai3', 4, 1);
+INSERT INTO `basket` (`id_basket`, `id_session`, `id_product`, `quantity`) VALUES
+(3, 'cpq9o34gk5arqa2rbhqdbdkak2', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -58,7 +49,7 @@ INSERT INTO `basket` (`id`, `id_session`, `id_product`, `quantity`) VALUES
 --
 
 CREATE TABLE `feedback` (
-  `id` int(11) NOT NULL,
+  `id_feedback` int(11) NOT NULL,
   `name` text NOT NULL,
   `text_fb` text NOT NULL,
   `date` datetime NOT NULL
@@ -68,12 +59,11 @@ CREATE TABLE `feedback` (
 -- Дамп данных таблицы `feedback`
 --
 
-INSERT INTO `feedback` (`id`, `name`, `text_fb`, `date`) VALUES
+INSERT INTO `feedback` (`id_feedback`, `name`, `text_fb`, `date`) VALUES
 (1, 'Вася', 'Отзыв 1', '2018-07-08 21:18:00'),
 (2, 'Иван', 'Отзыв 2', '2018-07-08 21:18:35'),
 (3, 'Алексей', 'Отзыв 3', '2018-07-08 21:22:14'),
-(4, 'Сергей', 'Отзыв 4', '2018-07-09 13:28:56'),
-(5, 'Потап', 'Отзыв 5', '2018-09-01 16:29:48');
+(4, 'Степан', 'Отзыв 4', '2018-09-09 20:08:02');
 
 -- --------------------------------------------------------
 
@@ -82,7 +72,6 @@ INSERT INTO `feedback` (`id`, `name`, `text_fb`, `date`) VALUES
 --
 
 CREATE TABLE `images` (
-  `id` int(11) NOT NULL,
   `id_image` int(11) NOT NULL,
   `path_img` text NOT NULL,
   `count_preview` int(11) NOT NULL,
@@ -94,22 +83,22 @@ CREATE TABLE `images` (
 -- Дамп данных таблицы `images`
 --
 
-INSERT INTO `images` (`id`, `id_image`, `path_img`, `count_preview`, `description`, `price`) VALUES
-(1, 1, '01.jpg', 16, 'Описание товара 1', '50000.00'),
-(2, 2, '02.jpg', 2, 'Описание товара 2', '75000.00'),
-(3, 3, '03.jpg', 14, 'Описание товара 3', '199000.90'),
-(4, 4, '04.jpg', 7, 'Описание товара 4', '999.99'),
-(5, 5, '05.jpg', 4, 'Описание товара 5', '1000.01'),
-(6, 6, '06.jpg', 4, 'Описание товара 6', '1000000.00'),
-(7, 7, '07.jpg', 3, 'Описание товара 7', '150000.00'),
-(8, 8, '08.jpg', 1, 'Описание товара 8', '10000000.00'),
-(9, 9, '09.jpg', 10, 'Описание товара 9', '35000.00'),
-(10, 10, '10.jpg', 1, 'Описание товара 10', '99999.99'),
-(11, 11, '11.jpg', 0, 'Описание товара 11', '350500.00'),
-(12, 12, '12.jpg', 3, 'Описание товара 12', '7800.00'),
-(13, 13, '13.jpg', 19, 'Описание товара 13', '4000000.00'),
-(14, 14, '14.jpg', 8, 'Описание товара 14', '140900.00'),
-(15, 15, '15.jpg', 1, 'Описание товара 15', '55700.00');
+INSERT INTO `images` (`id_image`, `path_img`, `count_preview`, `description`, `price`) VALUES
+(1, '01.jpg', 2, 'Описание товара 1', '50000.00'),
+(2, '02.jpg', 1, 'Описание товара 2', '75000.00'),
+(3, '03.jpg', 0, 'Описание товара 3', '199000.90'),
+(4, '04.jpg', 2, 'Описание товара 4', '999.99'),
+(5, '05.jpg', 0, 'Описание товара 5', '1000.01'),
+(6, '06.jpg', 0, 'Описание товара 6', '1000000.00'),
+(7, '07.jpg', 0, 'Описание товара 7', '150000.00'),
+(8, '08.jpg', 0, 'Описание товара 8', '10000000.00'),
+(9, '09.jpg', 0, 'Описание товара 9', '35000.00'),
+(10, '10.jpg', 0, 'Описание товара 10', '99999.99'),
+(11, '11.jpg', 0, 'Описание товара 11', '350500.00'),
+(12, '12.jpg', 0, 'Описание товара 12', '7800.00'),
+(13, '13.jpg', 0, 'Описание товара 13', '4000000.00'),
+(14, '14.jpg', 0, 'Описание товара 14', '140900.00'),
+(15, '15.jpg', 0, 'Описание товара 15', '55700.00');
 
 -- --------------------------------------------------------
 
@@ -131,7 +120,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id_user`, `login`, `password`, `email`) VALUES
 (1, 'admin', '123', ''),
 (2, 'user1', 'pass1', 'email1'),
-(3, 'user2', 'pass2', 'email2');
+(3, 'user2', 'pass2', 'email2'),
+(4, 'user3', 'pass3', 'email3');
 
 --
 -- Индексы сохранённых таблиц
@@ -141,19 +131,19 @@ INSERT INTO `users` (`id_user`, `login`, `password`, `email`) VALUES
 -- Индексы таблицы `basket`
 --
 ALTER TABLE `basket`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_basket`);
 
 --
 -- Индексы таблицы `feedback`
 --
 ALTER TABLE `feedback`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_feedback`);
 
 --
 -- Индексы таблицы `images`
 --
 ALTER TABLE `images`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_image`);
 
 --
 -- Индексы таблицы `users`
@@ -169,25 +159,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `basket`
 --
 ALTER TABLE `basket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id_basket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_feedback` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

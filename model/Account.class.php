670418c98705d7910ced0d8ddc40538c4032f321
AlexeyRadionov <radionov-aleksei@mail.ru>
2018-09-13 01:35:11 +0600

    добавил вывод заказов в личном кабинете

--- model/Account.class.php
+++ model/Account.class.php
@@ -1,17 +1,50 @@
 <?php
 	class Account extends Gallery {
 		private $back;
+		private $user;
 
 		public function getBack() {
 			return $this -> back;
 		}
 
-		public function setter($back) {
-			$this -> back = $back;
+		public function getUser() {
+			return $this -> user;
 		}
 
-		function __construct($back) {
-			$this -> setter($back);
+		public function setter($back, $user) {
+			$this -> back = $back;
+			$this -> user = $user;
+		}
+
+		function __construct($back, $user) {
+			$this -> setter($back, $user);
+		}
+
+		public function getInfoGoods() {
+			$user = $this -> user;
+			$sql = "SELECT `id_user` FROM users WHERE `login` = '$user'";
+			$login = $this -> getAssocResult($sql);
+			$id_user = $login[0]['id_user'];
+
+		    $sql = "SELECT * FROM `products_in_order`, `images`, `orders` WHERE orders.id_order = products_in_order.id_order AND `id_product` = `id_image` AND `id_user` = '$id_user' ORDER BY orders.id_order DESC";
+		    $goods = $this -> getAssocResult($sql);
+		    if (empty($goods)) {
+		        $goods = 'Пока у вас нет ни одного заказа';
+		    }
+		    
+		    return $goods;
+		}
+
+		public function getInfoOrders() {
+			$user = $this -> user;
+			$sql = "SELECT `id_user` FROM users WHERE `login` = '$user'";
+			$login = $this -> getAssocResult($sql);
+			$id_user = $login[0]['id_user'];
+
+			$sql = "SELECT DISTINCT `id_order`, `status`, `count`, `amount` FROM `orders` WHERE `id_user` = '$id_user' ORDER BY `id_order` DESC";
+			$infoOrders = $this -> getAssocResult($sql);
+
+			return $infoOrders;
 		}
 
 		public function template() {
@@ -29,7 +62,9 @@
 			  	'back_url' => $_SERVER['REQUEST_URI'],
 			  	'back' => $this -> back,
 			  	'auth' => Auth::$auth,
-			  	'user' => Auth::$auth
+			  	'user' => Auth::$auth,
+			  	'goods' => $this -> getInfoGoods(),
+			  	'infoOrders' => $this -> getInfoOrders()
 			  ));
 			  
 			} catch (Exception $e) {

2ffc667ef87708b7b4fb40250101a1e392392f9b
AlexeyRadionov <radionov-aleksei@mail.ru>
2018-09-12 11:40:24 +0600

    переименовал файл

--- controller/controllerAddFeedback.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if (isset($_GET['send'])) {
-		$name = $_GET['userName'];
-		$feedback = $_GET['feedback'];
-		$date = date('Y.m.d H:i:s');
-		$objAddFeedback = new AddFeedback($name, $feedback, $date);
-		$objAddFeedback -> addFeedback();
-	}
-?>

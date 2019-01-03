0e2c0b81ecf1efcc0d1a45d1c4d8bb1fe325e47e
AlexeyRadionov <radionov-aleksei@mail.ru>
2019-01-04 02:18:18 +0600

    добавил в базу данных связи таблиц через внешние ключи

--- data/gallery.sql
+++ data/gallery.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Сен 20 2018 г., 17:01
--- Версия сервера: 5.7.20
+-- Время создания: Янв 03 2019 г., 23:11
+-- Версия сервера: 5.7.20-log
 -- Версия PHP: 5.6.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -76,21 +76,21 @@ CREATE TABLE `images` (
 --
 
 INSERT INTO `images` (`id_image`, `path_img`, `count_preview`, `description`, `price`) VALUES
-(1, '01.jpg', 10, 'Описание товара 1', '50000.00'),
-(2, '02.jpg', 10, 'Описание товара 2', '75000.00'),
+(1, '01.jpg', 11, 'Описание товара 1', '50000.00'),
+(2, '02.jpg', 14, 'Описание товара 2', '75000.00'),
 (3, '03.jpg', 4, 'Описание товара 3', '199000.90'),
-(4, '04.jpg', 9, 'Описание товара 4', '999.99'),
-(5, '05.jpg', 16, 'Описание товара 5', '1000.01'),
-(6, '06.jpg', 2, 'Описание товара 6', '1000000.00'),
-(7, '07.jpg', 4, 'Описание товара 7', '150000.00'),
-(8, '08.jpg', 3, 'Описание товара 8', '10000000.00'),
-(9, '09.jpg', 4, 'Описание товара 9', '35000.00'),
-(10, '10.jpg', 6, 'Описание товара 10', '99999.99'),
-(11, '11.jpg', 3, 'Описание товара 11', '350500.00'),
-(12, '12.jpg', 4, 'Описание товара 12', '7800.00'),
-(13, '13.jpg', 1, 'Описание товара 13', '4000000.00'),
+(4, '04.jpg', 15, 'Описание товара 4', '999.99'),
+(5, '05.jpg', 18, 'Описание товара 5', '1000.01'),
+(6, '06.jpg', 5, 'Описание товара 6', '1000000.00'),
+(7, '07.jpg', 10, 'Описание товара 7', '150000.00'),
+(8, '08.jpg', 6, 'Описание товара 8', '10000000.00'),
+(9, '09.jpg', 11, 'Описание товара 9', '35000.00'),
+(10, '10.jpg', 11, 'Описание товара 10', '99999.99'),
+(11, '11.jpg', 12, 'Описание товара 11', '350500.00'),
+(12, '12.jpg', 5, 'Описание товара 12', '7800.00'),
+(13, '13.jpg', 6, 'Описание товара 13', '4000000.00'),
 (14, '14.jpg', 2, 'Описание товара 14', '140900.00'),
-(15, '15.jpg', 5, 'Описание товара 15', '55700.00');
+(15, '15.jpg', 11, 'Описание товара 15', '55700.00');
 
 -- --------------------------------------------------------
 
@@ -113,13 +113,15 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id_order`, `id_session`, `id_user`, `status`, `count`, `amount`, `date`) VALUES
-(8, 'j943hr9jiqgjoaarie0v9jadt6', 2, 'Принят в работу', 3, '77000.02', '2018-09-12 16:02:01'),
-(9, 'j943hr9jiqgjoaarie0v9jadt6', 2, 'Принят в работу', 1, '7800.00', '2018-09-12 16:03:27'),
+(8, 'j943hr9jiqgjoaarie0v9jadt6', 2, 'В обработке', 3, '77000.02', '2018-09-12 16:02:01'),
+(9, 'j943hr9jiqgjoaarie0v9jadt6', 2, 'В обработке', 1, '7800.00', '2018-09-12 16:03:27'),
 (10, 'j943hr9jiqgjoaarie0v9jadt6', 3, 'В обработке', 1, '55700.00', '2018-09-12 16:08:31'),
-(11, 'j943hr9jiqgjoaarie0v9jadt6', 3, 'Отправлен', 2, '8000000.00', '2018-09-12 16:10:25'),
+(11, 'j943hr9jiqgjoaarie0v9jadt6', 3, 'В обработке', 2, '8000000.00', '2018-09-12 16:10:25'),
 (12, 'j943hr9jiqgjoaarie0v9jadt6', 3, 'В обработке', 4, '290699.98', '2018-09-12 22:27:23'),
-(13, '72bf4tvn413r3oopr3t500im04', 4, 'Отправлен', 2, '1350500.00', '2018-09-17 19:40:19'),
-(14, '72bf4tvn413r3oopr3t500im04', 2, 'Принят в работу', 1, '999.99', '2018-09-17 20:50:18');
+(13, '72bf4tvn413r3oopr3t500im04', 4, 'В обработке', 2, '1350500.00', '2018-09-17 19:40:19'),
+(14, '72bf4tvn413r3oopr3t500im04', 2, 'В обработке', 1, '999.99', '2018-09-17 20:50:18'),
+(48, '9revsj62lvlk5mhoq81u33in34', 5, 'Принят в работу', 1, '35000.00', '2019-01-03 22:46:05'),
+(49, 's3satk9crjp0g740ptonp8d7o2', 5, 'В обработке', 1, '10000000.00', '2019-01-03 23:08:23');
 
 -- --------------------------------------------------------
 
@@ -149,7 +151,9 @@ INSERT INTO `products_in_order` (`id_products_in_order`, `id_order`, `id_product
 (18, 12, 9, 1),
 (19, 13, 6, 1),
 (20, 13, 11, 1),
-(21, 14, 4, 1);
+(21, 14, 4, 1),
+(55, 48, 9, 1),
+(56, 49, 8, 1);
 
 -- --------------------------------------------------------
 
@@ -183,7 +187,8 @@ INSERT INTO `users` (`id_user`, `login`, `password`, `email`) VALUES
 -- Индексы таблицы `basket`
 --
 ALTER TABLE `basket`
-  ADD PRIMARY KEY (`id_basket`);
+  ADD PRIMARY KEY (`id_basket`),
+  ADD KEY `basket_fk_images` (`id_product`);
 
 --
 -- Индексы таблицы `feedback`
@@ -201,13 +206,16 @@ ALTER TABLE `images`
 -- Индексы таблицы `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id_order`);
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `orders_fk_users` (`id_user`);
 
 --
 -- Индексы таблицы `products_in_order`
 --
 ALTER TABLE `products_in_order`
-  ADD PRIMARY KEY (`id_products_in_order`);
+  ADD PRIMARY KEY (`id_products_in_order`),
+  ADD KEY `products_in_order_fk_orders` (`id_order`),
+  ADD KEY `products_in_order_fk_images` (`id_product`);
 
 --
 -- Индексы таблицы `users`
@@ -223,7 +231,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `basket`
 --
 ALTER TABLE `basket`
-  MODIFY `id_basket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_basket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `feedback`
@@ -241,19 +249,41 @@ ALTER TABLE `images`
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT для таблицы `products_in_order`
 --
 ALTER TABLE `products_in_order`
-  MODIFY `id_products_in_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_products_in_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `basket`
+--
+ALTER TABLE `basket`
+  ADD CONSTRAINT `basket_fk_images` FOREIGN KEY (`id_product`) REFERENCES `images` (`id_image`);
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_fk_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Ограничения внешнего ключа таблицы `products_in_order`
+--
+ALTER TABLE `products_in_order`
+  ADD CONSTRAINT `products_in_order_fk_images` FOREIGN KEY (`id_product`) REFERENCES `images` (`id_image`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
